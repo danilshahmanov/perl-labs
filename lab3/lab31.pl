7413196424c05a08940f05dae7969143a99d91f9
danilshahmanov <daniltopskij174@gmail.com>
2024-09-20 10:02:29 +0300

    fix: lab3

--- lab3/lab31.pl
+++ lab3/lab31.pl
@@ -1,35 +1,31 @@
-#!/usr/bin/perl
 use strict;
 use warnings;
 
-# Основная функция
-sub main {
-    my $head = undef; # Начальный элемент списка (пустой)
+my $head = undef;  # Начальный элемент списка (пустой)
 
-    while (1) {
-        print_menu();
-        my $menu = <STDIN>;
-        chomp($menu);
+while (1) {
+    print_menu();
+    my $menu = <STDIN>;
+    chomp($menu);
 
-        if ($menu == 1) {
-            add_student(\$head);
-        } elsif ($menu == 2) {
-            remove_student(\$head);
-        } elsif ($menu == 3) {
-            print_list($head);
-        } elsif ($menu == 4) {
-            last;
-        } else {
-            print "Нет такой команды\n";
-        }
+    if ($menu == 1) {
+        add_student(\$head);
+    } elsif ($menu == 2) {
+        remove_student(\$head);
+    } elsif ($menu == 3) {
+        print_list(\$head);
+    } elsif ($menu == 4) {
+        last;
+    } else {
+        print "Нет такой команды\n";
     }
 }
 
 # Печать меню команд
 sub print_menu {
     print "Меню:\n";
-    print "1. Добавить строку в список\n";
-    print "2. Удалить строку из списка\n";
+    print "1. Добавить студента в список\n";
+    print "2. Удалить студента из списка\n";
     print "3. Просмотреть список\n";
     print "4. Выход\n";
     print "Введите номер команды: ";
@@ -62,24 +58,6 @@ sub add_student {
     add($head_ref, $fio, $book_number, $group_number, $spec, $birth);
 }
 
-# Удаление студента из списка
-sub remove_student {
-    my ($head_ref) = @_;
-
-    print "Введите номер зачетной книжки: ";
-    my $book_number = <STDIN>;
-    chomp($book_number);
-
-    remove($head_ref, $book_number);
-}
-
-# Печать списка студентов
-sub print_list {
-    my ($head) = @_;
-    print "\nФИО Зачётка Группа Специальность Год рождения\n";
-    output($head);
-}
-
 # Добавление записи в список
 sub add {
     my ($head_ref, $fio, $book_number, $group_number, $spec, $birth) = @_;
@@ -92,48 +70,87 @@ sub add {
         NEXT => undef
     };
 
+    # Если список пуст
     if (!$$head_ref) {
         $$head_ref = $record;
         return;
     }
 
-    if ($$head_ref->{book_number} eq $book_number) {
-        print "Студент уже в списке\n";
-        return;
-    } elsif ($$head_ref->{book_number} lt $book_number) {
-        add(\$$head_ref->{NEXT}, $fio, $book_number, $group_number, $spec, $birth);
-    } else {
+    # Если новый элемент должен быть в начале
+    if ($$head_ref->{book_number} gt $book_number) {
         $record->{NEXT} = $$head_ref;
         $$head_ref = $record;
+        return;
     }
+
+    # Найти место для вставки элемента
+    my $current = $$head_ref;
+    while ($current->{NEXT} && $current->{NEXT}->{book_number} lt $book_number) {
+        $current = $current->{NEXT};
+    }
+
+    if ($current->{book_number} eq $book_number) {
+        print "Студент с таким номером зачетной книжки уже есть в списке\n";
+        return;
+    }
+
+    $record->{NEXT} = $current->{NEXT};
+    $current->{NEXT} = $record;
 }
 
 # Удаление записи из списка
+sub remove_student {
+    my ($head_ref) = @_;
+
+    print "Введите номер зачетной книжки: ";
+    my $book_number = <STDIN>;
+    chomp($book_number);
+
+    remove($head_ref, $book_number);
+}
+
 sub remove {
     my ($head_ref, $book_number) = @_;
 
     if (!$$head_ref) {
-        print "Студент не найден\n";
+        print "Список пуст\n";
         return;
     }
 
+    # Удаление из начала списка
     if ($$head_ref->{book_number} eq $book_number) {
         $$head_ref = $$head_ref->{NEXT};
+        print "Студент удалён\n";
         return;
     }
 
-    remove(\$$head_ref->{NEXT}, $book_number);
-}
+    # Поиск записи для удаления
+    my $current = $$head_ref;
+    while ($current->{NEXT} && $current->{NEXT}->{book_number} ne $book_number) {
+        $current = $current->{NEXT};
+    }
 
-# Печать списка записей
-sub output {
-    my ($record) = @_;
-
-    while ($record) {
-        print "$record->{fio} -> $record->{book_number} -> $record->{group_number} -> $record->{spec} -> $record->{birth}\n";
-        $record = $record->{NEXT};
+    if ($current->{NEXT}) {
+        $current->{NEXT} = $current->{NEXT}->{NEXT};
+        print "Студент удалён\n";
+    } else {
+        print "Студент не найден\n";
     }
 }
 
-# Запуск основной функции
-main();
+# Печать списка студентов
+sub print_list {
+    my ($head_ref) = @_;
+
+    if (!$$head_ref) {
+        print "Список пуст.\n";
+        return;
+    }
+
+    print "\nФИО -> Зачётка -> Группа -> Специальность -> Год рождения\n";
+    my $current = $$head_ref;
+    while ($current) {
+        print "$current->{fio} -> $current->{book_number} -> $current->{group_number} -> $current->{spec} -> $current->{birth}\n";
+        $current = $current->{NEXT};
+    }
+}
